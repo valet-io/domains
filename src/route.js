fc2652e70b836f6b3f535dca093015fe441e82cf
Ben Drucker <bvdrucker@gmail.com>
2014-09-25 14:55:56 -0400

    Revert "Send an empty reply when there's no host"
    This reverts commit 7278409163683e6828e0b57a3294d9464aa380a2.

--- src/route.js
+++ src/route.js
@@ -14,20 +14,15 @@ module.exports = function (server) {
     method: '*',
     path: '/{p*}',
     handler: function (request, reply) {
-      if (!request.info.host) {
-        return reply();
-      }
-      else {
-        var host = request.info.host.replace(/^www\./, '');
-        server.log(['redirect'], {
-          from: host,
-          www_stripped: request.info.host !== host
-        });
-        server.methods.getLocation(host, function (err, location) {
-          if (err) return reply(err);
-          return reply().redirect(location);
-        });
-      }
+      var host = request.info.host.replace(/^www\./, '');
+      server.log(['redirect'], {
+        from: host,
+        www_stripped: request.info.host !== host
+      });
+      server.methods.getLocation(host, function (err, location) {
+        if (err) return reply(err);
+        return reply().redirect(location);
+      });
     }
   });
 
