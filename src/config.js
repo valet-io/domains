889bb2f679d72a9677722cc1acaf9d8b2f1af4c6
Ben Drucker <bvdrucker@gmail.com>
2015-02-06 09:27:20 -0500

    Provide environment variables for configuration

--- src/config.js
+++ src/config.js
@@ -3,22 +3,28 @@
 module.exports = require('convict')({
   templates: {
     api: {
+      env: 'API_TEMPLATE',
       format: String,
       default: null
     },
     projector: {
+      env: 'PROJECTOR_TEMPLATE',
       format: String,
       default: null
     },
     pledge: {
+      env: 'PLEDGE_TEMPLATE',
       format: String,
       default: null
     }
   },
   port: {
+    env: 'PORT',
+    format: Number,
     default: 0
   },
   host: {
+    env: 'HOST',
     format: String,
     default: 'localhost'
   }
